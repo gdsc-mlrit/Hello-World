2aa6f5394928a92cd19c96d8975aaba4613ad20d
Abhiram <vemulaabhiram2004@gmail.com>
2023-10-20 17:44:18 +0530

    changed hero

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -5,15 +5,19 @@ const Hero = ({ dark }: any) => {
     <section
       className={`min-h-[90vh] ${
         dark ? "bg-[#151515] text-white" : "bg-white"
-      } bg-cover flex justify-start items-center w-full relative h-full max-sm:bg-[url(/mobilebg.png)] max-sm:bg-cover`}
+      } bg-cover flex justify-start items-center w-full relative h-full`}
     >
-      <div className="max-sm:min-h-[75vh] max-sm:flex max-sm:flex-col max-sm:gap-[2rem] max-sm:justify-center md:max-w-[60%] pl-8 lg:pl-[9rem] md:justify-center md:items-center z-10 flex-1">
+      <div className="max-sm:min-h-[75vh] max-sm:flex max-sm:pt-[6rem] max-sm:flex-col max-sm:gap-[2rem] max-sm:justify-center md:max-w-[60%] pl-8 lg:pl-[9rem] md:justify-center md:items-center z-10 flex-1 relative">
         <h1 className="md:text-[12em] sm:text-[9rem] font-semibold sm:leading-[9rem] md:leading-[12rem] text-[6rem] leading-[6rem] font-poppins pb-5 z-10">
           Hello, <br />
           <span>
             World<span className="text-[#99F94E] ">.</span>
           </span>
         </h1>
+
+        <div className="sm:hidden absolute top-0 right-0 w-[200px]">
+          <img src="/HeroImage.png" alt="globe" />
+        </div>
 
         <p className="sm:hidden font-semibold pt-5 text-[23px]">
           Dive into the emerging <br />
