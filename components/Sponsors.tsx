cd9548f0ce569959a617af0cd67c5604dd2d5003
Abhiram <vemulaabhiram2004@gmail.com>
2023-11-10 06:40:52 +0530

    ....

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -5,7 +5,7 @@ const Sponsors = () => {
     <div className=" w-full px-6 bg-[#151515] py-10 md:pt-[150px]">
       <div className="text-center h-auto flex flex-col items-center m-auto">
         <div>
-          <h1 className="text-limee leading-[30px] text-center text-[30px] md:text-[50px] md:leading-[50px] lg:text-[70px] lg:leading-[70px] font-bold">
+          <h1 className="text-limee leading-[40px] text-center text-[40px] md:text-[60px] md:leading-[60px] lg:text-[80px] lg:leading-[80px] font-bold mb-10">
             Our Sponsors
           </h1>
         </div>
@@ -21,49 +21,60 @@ const Sponsors = () => {
             />
           </a>
         </div> */}
+        <div className="flex flex-col justify-center items-center gap-10 w-full">
+          <div className="flex flex-col justify-center items-center w-full  max-md:gap-5">
+            <h2 className="lg:text-[3rem] md:text-[2.5rem] text-[2rem]  text-white  font-semibold lg:pl-3">
+              Gold Partner
+            </h2>
+            <div className="flex flex-wrap gap-10 w-[75%] justify-center rounded-3xl p-10 bg-[#1f1f1f]">
+              <a href="https://www.jdoodle.com/" target="_blank" className="">
+                <img
+                  src="/JD_logo.png"
+                  alt=""
+                  className="lg:h-[10rem] md:h-[7rem] h-[5rem]"
+                />
+              </a>
+            </div>
+          </div>
 
-        <h2 className="lg:text-[3.5rem] md:text-[3rem] text-[2.5rem] bg-gradient-to-r to-yellow-300 from-[#9c7623] text-transparent bg-clip-text font-semibold lg:pl-3">
-          Gold Partner
-        </h2>
-        <div className="flex flex-wrap gap-10 w-[75%] justify-center rounded-3xl p-10 bg-[#1f1f1f]">
-          <a href="https://www.jdoodle.com/" target="_blank" className="">
-            <img
-              src="/JD_logo.png"
-              alt=""
-              className="lg:h-[10rem] md:h-[7rem] h-[5rem]"
-            />
-          </a>
+          <div className="flex flex-col justify-center items-center w-full  max-md:gap-5">
+            <h2 className="lg:text-[3rem] md:text-[2.5rem] text-[2rem] font-semibold lg:pl-3 text-white">
+              Bronze Partner
+            </h2>
+            <div className="flex flex-wrap gap-10 w-[75%] justify-center rounded-3xl p-10 bg-[#1f1f1f] m-auto">
+              <a
+                href="https://beeceptor.com/?utm_source=gdsc"
+                target="_blank"
+                className="flex items-center p-5"
+              >
+                <img src="beer.svg" alt="" className="w-[22rem]" />
+              </a>
+
+              <a href="https://interviewbuddy.net/" target="_blank">
+                <img src="iblogo.png" alt="" className="w-[22rem]" />
+              </a>
+
+              <a
+                href="https://company.wolfram.com/press-center/wolfram-corporate/https://interviewbuddy.net/"
+                target="_blank"
+                className="flex items-center"
+              >
+                <img src="wolfram2.png" alt="" className="w-[22rem]" />
+              </a>
+            </div>
+          </div>
+
+          <div className="flex flex-col justify-center items-center w-full max-md:gap-5">
+            <h2 className="lg:text-[3rem] md:text-[2.5rem] text-[2rem] font-semibold lg:pl-3 text-white">
+              Knowledge Partner
+            </h2>
+            <div className="flex flex-wrap gap-10 w-[75%] justify-center rounded-3xl p-10 bg-[#1f1f1f] m-auto">
+              <a href="https://cie.iiit.ac.in/" target="_blank">
+                <img src="IIIT-CIE-Logo.png" alt="" className="w-[22rem]" />
+              </a>
+            </div>
+          </div>
         </div>
-
-        <h2 className="lg:text-[3.5rem] md:text-[3rem] text-[2.5rem] font-semibold lg:pl-3 bg-gradient-to-r to-[#d19200] from-[#7a5507] bg-clip-text text-transparent">
-          Bronze Partner
-        </h2>
-        <div className="flex flex-wrap gap-10 w-[75%] justify-center rounded-3xl p-10 bg-[#1f1f1f] m-auto">
-          <a
-            href="https://beeceptor.com/?utm_source=gdsc"
-            target="_blank"
-            className="flex items-center p-5"
-          >
-            <img src="beer.svg" alt="" className="w-[22rem]" />
-          </a>
-
-          <a href="https://interviewbuddy.net/" target="_blank">
-            <img src="iblogo.png" alt="" className="w-[22rem]" />
-          </a>
-
-          <a
-            href="https://company.wolfram.com/press-center/wolfram-corporate/https://interviewbuddy.net/"
-            target="_blank"
-            className="flex items-center"
-          >
-            <img src="wolfram2.png" alt="" className="w-[22rem]" />
-          </a>
-        </div>
-
-        <h2 className="lg:text-[3.5rem] md:text-[3rem] text-[2.5rem] font-semibold lg:pl-3 bg-gradient-to-r to-[#d19200] from-[#7a5507] bg-clip-text text-transparent">
-          Knowledge Partner
-        </h2>
-        <div className="flex flex-wrap gap-10 w-[75%] justify-center rounded-3xl p-10 bg-[#1f1f1f] m-auto"></div>
 
         {/* <div className="flex max-sm:flex-col max-sm:justify-center sm:justify-between items-center rounded-2xl bg-[#1f1f1f] lg:w-[75%] w-full p-3 mt-10 max-lg:px-10">
           <h2 className="lg:text-[3.5rem] md:text-[3rem] text-[2.5rem] bg-gradient-to-r from-gray-500 to-gray-100 text-transparent bg-clip-text font-semibold lg:pl-3">
